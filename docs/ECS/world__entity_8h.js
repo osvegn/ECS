638d275e0bf2a9721ade447934de2330bb8678bf
osvegn <thomas.prudhomme@epitech.eu>
2023-03-13 14:25:03 +0000

    Deploying to gh-pages from @ osvegn/ECS@cde6c9cc129ab73ce2034ab274c93f31e51287d0 🚀

--- docs/ECS/world__entity_8h.js
+++ docs/ECS/world__entity_8h.js
@@ -2,7 +2,9 @@ var world__entity_8h =
 [
     [ "world_add_entity", "world__entity_8h.html#ae769993c3f5d70f3d8edc10073495cd3", null ],
     [ "world_contains_entity", "world__entity_8h.html#a76248669a6b79823d4741754f2e4a41a", null ],
+    [ "world_contains_entity_by_id", "world__entity_8h.html#acdf2df2afa49ee00b089e3334e84e8e5", null ],
     [ "world_get_entity_by_id", "world__entity_8h.html#a0414d6ea7fbc43bbb94f129e20219ff5", null ],
     [ "world_join_entities", "world__entity_8h.html#ab7ff0b58102c05b45a6722f11c9a3d2b", null ],
-    [ "world_remove_entity", "world__entity_8h.html#a53c47319d553d6f0cc40bac726a9abe9", null ]
+    [ "world_remove_entity", "world__entity_8h.html#a53c47319d553d6f0cc40bac726a9abe9", null ],
+    [ "world_remove_entity_by_id", "world__entity_8h.html#a018f4fc9652790fe22ba4d75f91d6f30", null ]
 ];
