fee08a19f34f3c05ad0c8c36c3b6649f4480aa14
osvegn <thomas.prudhomme@epitech.eu>
2023-03-21 16:41:19 +0000

    Deploying to gh-pages from @ osvegn/ECS@6aa83a51973a1ad434c1514db748a0479939f46b 🚀

--- docs/ECS/structresource__s.js
+++ docs/ECS/structresource__s.js
@@ -1,5 +1,6 @@
 var structresource__s =
 [
     [ "data", "structresource__s.html#a735984d41155bc1032e09bece8f8d66d", null ],
+    [ "destructor", "structresource__s.html#a716f180892d5048cd8ee82ab0c7602f6", null ],
     [ "type", "structresource__s.html#a4bfea42429249a1f65204f0c0f34704a", null ]
 ];
