d64de2ad1ff82a4aeff28eeea0f8229664740dbc
osvegn <thomas.prudhomme@epitech.eu>
2023-04-16 17:03:24 +0000

    Deploying to gh-pages from @ osvegn/ECS@3e6added38ac7a60039643dea958612cc6522c6f 🚀

--- docs/ECS/entity_8h.js
+++ docs/ECS/entity_8h.js
@@ -5,6 +5,7 @@ var entity_8h =
     [ "entity_add_component", "entity_8h.html#ae9c4710107a012ef60c252072a5dd5b9", null ],
     [ "entity_constructor", "entity_8h.html#aa4d524c8b6d170b4b513842e4d858c63", null ],
     [ "entity_contains_component", "entity_8h.html#a047dda0071c76403873c41814fe77c20", null ],
+    [ "entity_contains_component_by_type", "entity_8h.html#a0fc2f14b15d533482f2180f149afc72b", null ],
     [ "entity_destructor", "entity_8h.html#aa52c87ef09bcc4c44121c235b1a77244", null ],
     [ "entity_get_component", "entity_8h.html#afc477fdf5c88e639c50f163067c84053", null ],
     [ "entity_remove_component", "entity_8h.html#ac0e9402cb9be7aa3d6e10cb7e81c3124", null ]
