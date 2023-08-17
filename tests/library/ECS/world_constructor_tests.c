90a18cf752472639678dad4d16000b66345d2e4f
osvegn <thomas.prudhomme@epitech.eu>
2023-08-17 23:44:01 +0200

    [FIX] world_constructor tests syntax

--- tests/library/ECS/world_constructor_tests.c
+++ tests/library/ECS/world_constructor_tests.c
@@ -26,11 +26,11 @@ Test(world_destructor, world_destructor_with_entity)
 
     entity_constructor(&entity);
     entity_add_component(&entity, &component);
-    world->entity_list.emplace_back(&world->entity_list, &entity);
+    world.entity_list.emplace_back(&world.entity_list, &entity);
     world_constructor(&world, stdout);
-    cr_assert_eq(world->entity_list.size(&world->entity_list), 1);
+    cr_assert_eq(world.entity_list.size(&world.entity_list), 1);
     world_destructor(&world);
-    cr_assert_eq(world->entity_list.size(&world->entity_list), 0);
+    cr_assert_eq(world.entity_list.size(&world.entity_list), 0);
 }
 
 Test(world_destructor, world_destructor_with_resource)
@@ -40,7 +40,7 @@ Test(world_destructor, world_destructor_with_resource)
 
     world_add_resource(&world, &resource);
     world_constructor(&world, stdout);
-    cr_assert_eq(world->resource_list.size(&world->resource_list), 1);
+    cr_assert_eq(world.resource_list.size(&world.resource_list), 1);
     world_destructor(&world);
-    cr_assert_eq(world->resource_list.size(&world->resource_list), 0);
+    cr_assert_eq(world.resource_list.size(&world.resource_list), 0);
 }
