1347f2abf470d939457bbca9617c2a0c4263b7a6
osvegn <thomas.prudhomme@epitech.eu>
2023-08-24 21:02:10 +0000

    Deploying to gh-pages from @ osvegn/ECS@e1c17d52f2300efd595785235c3475f0fcc2313f 🚀

--- docs/ECS/entity_8h.js
+++ docs/ECS/entity_8h.js
@@ -7,6 +7,8 @@ var entity_8h =
     [ "entity_contains_component", "entity_8h.html#a047dda0071c76403873c41814fe77c20", null ],
     [ "entity_contains_component_by_type", "entity_8h.html#a0fc2f14b15d533482f2180f149afc72b", null ],
     [ "entity_destructor", "entity_8h.html#aa52c87ef09bcc4c44121c235b1a77244", null ],
-    [ "entity_get_component", "entity_8h.html#afc477fdf5c88e639c50f163067c84053", null ],
-    [ "entity_remove_component", "entity_8h.html#ac0e9402cb9be7aa3d6e10cb7e81c3124", null ]
+    [ "entity_get_component", "entity_8h.html#a32dd0a41a77bb5393ba2d25763359a83", null ],
+    [ "entity_get_component_by_type", "entity_8h.html#a3ef7b2bc2f1bd488f2ba8ab82726f4df", null ],
+    [ "entity_remove_component", "entity_8h.html#ac0e9402cb9be7aa3d6e10cb7e81c3124", null ],
+    [ "entity_remove_component_by_type", "entity_8h.html#aa3cfc8f8153668edb43e6a5e8fc51613", null ]
 ];
--- docs/ECS/entity__component_8c.js
+++ docs/ECS/entity__component_8c.js
@@ -5,6 +5,8 @@ var entity__component_8c =
     [ "entity_contains_component", "entity__component_8c.html#a047dda0071c76403873c41814fe77c20", null ],
     [ "entity_contains_component_by_type", "entity__component_8c.html#a0fc2f14b15d533482f2180f149afc72b", null ],
     [ "entity_destructor", "entity__component_8c.html#aa52c87ef09bcc4c44121c235b1a77244", null ],
-    [ "entity_get_component", "entity__component_8c.html#afc477fdf5c88e639c50f163067c84053", null ],
-    [ "entity_remove_component", "entity__component_8c.html#ac0e9402cb9be7aa3d6e10cb7e81c3124", null ]
+    [ "entity_get_component", "entity__component_8c.html#a32dd0a41a77bb5393ba2d25763359a83", null ],
+    [ "entity_get_component_by_type", "entity__component_8c.html#a3ef7b2bc2f1bd488f2ba8ab82726f4df", null ],
+    [ "entity_remove_component", "entity__component_8c.html#ac0e9402cb9be7aa3d6e10cb7e81c3124", null ],
+    [ "entity_remove_component_by_type", "entity__component_8c.html#aa3cfc8f8153668edb43e6a5e8fc51613", null ]
 ];
