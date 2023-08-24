a053a47112b948599635f124554262e6f70c15e6
osvegn <thomas.prudhomme@epitech.eu>
2023-08-24 23:21:04 +0200

    [UPD] improve world_system functions

--- source/world_system.c
+++ source/world_system.c
@@ -9,19 +9,6 @@
 
 #include "world.h"
 
-/// @brief It finds the index of a system in a list of systems
-/// @param system_list The list of systems to search through.
-/// @param system The system to find.
-/// @return The index of the system in the system_list vector, or list size if not found.
-static unsigned int find_system(vector_t *system_list, system_t *system)
-{
-    for (unsigned int i = 0; i < system_list->size(system_list); i++) {
-        if (system->type == (*(system_t *)(system_list->at(system_list, i))).type)
-            return i;
-    }
-    return system_list->size(system_list);
-}
-
 static unsigned int find_system_by_type(vector_t *system_list, unsigned int type)
 {
     for (unsigned int i = 0; i < system_list->size(system_list); i++) {
@@ -30,6 +17,15 @@ static unsigned int find_system_by_type(vector_t *system_list, unsigned int type
         }
     }
     return system_list->size(system_list);
+}
+
+/// @brief It finds the index of a system in a list of systems
+/// @param system_list The list of systems to search through.
+/// @param system The system to find.
+/// @return The index of the system in the system_list vector, or list size if not found.
+static unsigned int find_system(vector_t *system_list, system_t *system)
+{
+    return find_system_by_type(system_list, system->type);
 }
 
 int world_add_system(world_t *world, system_t *system)
@@ -43,64 +39,40 @@ int world_add_system(world_t *world, system_t *system)
     return 0;
 }
 
-int world_remove_system(world_t *world, system_t *system)
-{
-    unsigned int index = find_system(&world->system_list, system);
-
-    if (index < world->system_list.size(&world->system_list)) {
-        world->system_list.erase(&world->system_list, index);
-        return 0;
-    }
-    return -1;
-}
-
 int world_remove_system_by_type(world_t *world, unsigned int type)
 {
     unsigned int index = find_system_by_type(&world->system_list, type);
 
-    if (index < world->system_list.size(&world->system_list)) {
-        world->system_list.erase(&world->system_list, index);
-        return 0;
-    }
-    return -1;
+    return (world->system_list.erase(&world->system_list, index));
+
 }
 
-bool world_contains_system(world_t *world, system_t *system)
+int world_remove_system(world_t *world, system_t *system)
 {
-    unsigned int index = find_system(&world->system_list, system);
-
-    if (index < world->system_list.size(&world->system_list)) {
-        return true;
-    }
-    return false;
+    return world_remove_system_by_type(world, system->type);
 }
 
 bool world_contains_system_by_type(world_t *world, unsigned int type)
 {
     unsigned int index = find_system_by_type(&world->system_list, type);
+    unsigned int size = world->system_list.size(&world->system_list);
 
-    if (index < world->system_list.size(&world->system_list)) {
-        return true;
-    }
-    return false;
+    return (index < size);
 }
 
-system_t *world_get_system(world_t *world, system_t *system)
+bool world_contains_system(world_t *world, system_t *system)
 {
-    unsigned int index = find_system(&world->system_list, system);
-
-    if (index < world->system_list.size(&world->system_list)) {
-        return world->system_list.at(&world->system_list, index);
-    }
-    return 0;
+    return (world_contains_system_by_type(world, system->type));
 }
 
 system_t *world_get_system_by_type(world_t *world, unsigned int type)
 {
     unsigned int index = find_system_by_type(&world->system_list, type);
 
-    if (index < world->system_list.size(&world->system_list)) {
-        return world->system_list.at(&world->system_list, index);
-    }
-    return 0;
+    return world->system_list.at(&world->system_list, index);
+}
+
+system_t *world_get_system(world_t *world, system_t *system)
+{
+    return (world_get_system_by_type(world, system->type));
 }
